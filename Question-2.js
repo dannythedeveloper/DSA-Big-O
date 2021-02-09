2744a47050c40974e074b85556b1441bf3ee7ef0
Danny Carcamo <dannycarcamo@icloud.com>
2021-02-09 11:01:13 -0600

    2nd question complete

---
+++ Question-2.js
@@ -0,0 +1,14 @@
+// 2. Even or odd
+
+// What is the Big O of the following algorithm? Explain your answer
+
+/* function isEven(value) {
+    if (value % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+} */
+
+// O(1) Constant, always has one input and one output 
